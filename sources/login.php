b56f9e0d22c5cf142d36e31e6cf48f4c5f26d6b4
Quanghyy1406 <nguyenquanghuy1023@gmail.com>
2025-09-25 16:39:05 +0700

    Update code to prevent XSS

---
+++ sources/login.php
@@ -0,0 +1,97 @@
+<?php
+// Start the session
+session_start();
+
+require_once 'models/UserModel.php';
+$userModel = new UserModel();
+
+if (!empty($_POST['submit'])) {
+    $users = [
+        'username' => $_POST['username'],
+        'password' => $_POST['password']
+    ];
+    $user = NULL;
+    if ($user = $userModel->auth($users['username'], $users['password'])) {
+        //Login successful
+        $_SESSION['id'] = $user[0]['id'];
+
+        $_SESSION['message'] = 'Login successful';
+        header('location: list_users.php');
+        exit();
+    }else {
+        //Login failed
+        $_SESSION['message'] = 'Login failed for user: ' . $_POST['username'];
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User form</title>
+    <?php include 'views/meta.php' ?>
+</head>
+<body>
+<?php include 'views/header.php'?>
+
+    <div class="container">
+        <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+            <div class="panel panel-info" >
+                <div class="panel-heading">
+                    <div class="panel-title">Login</div>
+                    <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot password?</a></div>
+                </div>
+
+                <div style="padding-top:30px" class="panel-body" >
+                    <!-- Hiển thị thông báo lỗi (đã chống XSS) -->
+                    <?php if (!empty($_SESSION['message'])): ?>
+                        <div class="alert alert-info">
+                            <?php
+                                echo htmlspecialchars($_SESSION['message']);
+                                unset($_SESSION['message']);
+                            ?>
+                        </div>
+                    <?php endif; ?>
+
+                    <form method="post" class="form-horizontal" role="form">
+
+                        <div class="margin-bottom-25 input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <!-- Hiển thị lại username (đã chống XSS) -->
+                            <input id="login-username" type="text" class="form-control" name="username" value="<?php echo isset($_POST['username']) ? $_POST['username'] : ''; ?>" placeholder="username or email">
+                        </div>
+
+                        <div class="margin-bottom-25 input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                            <input id="login-password" type="password" class="form-control" name="password" placeholder="password">
+                        </div>
+
+                        <div class="margin-bottom-25">
+                            <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
+                            <label for="remember"> Remember Me</label>
+                        </div>
+
+                        <div class="margin-bottom-25 input-group">
+                            <!-- Button -->
+                            <div class="col-sm-12 controls">
+                                <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+                                <a id="btn-fblogin" href="#" class="btn btn-primary">Login with Facebook</a>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-12 control">
+                                    Don't have an account!
+                                    <a href="form_user.php">
+                                        Sign Up Here
+                                    </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
